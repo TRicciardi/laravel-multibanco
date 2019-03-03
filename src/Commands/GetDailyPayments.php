e1b663e1cccd19d5724e1ac1e765e977f37744a6
Tiago Ricciardi <tiago.ricciardi@browserbox.pt>
2019-03-03 12:25:37 +0000

    fix getdaily command

--- src/Commands/GetDailyPayments.php
+++ src/Commands/GetDailyPayments.php
@@ -72,56 +72,58 @@ class GetDailyPayments extends Command
 
       $xml = $response->getBody() ;
       $payments =  xml_string_to_array($xml);
-
       //extract all references
-      $notifications = $payments['ref_detail']['ref'];
+      if(isset($payments['ref_detail'])) {
 
-      //for each reference, check if its notification is on the notifications table.
-      //if not, add to table
-      foreach($notifications as $payment) {
-        $not = EasypayNotification::where('ep_doc',$payment['ep_doc'])->first();
-        if(!$not) {
-          $not = new EasypayNotification;
-          $not->ep_doc = $payment['ep_doc'];
-          $not->ep_cin = $payment['ep_cin'];
-          $not->ep_user = $payment['ep_user'];
+        $notifications = $payments['ref_detail']['ref'];
 
-          $not->ep_status = 'ok0';
-          $not->save();
-        }
+        //for each reference, check if its notification is on the notifications table.
+        //if not, add to table
+        foreach($notifications as $payment) {
+          $not = EasypayNotification::where('ep_doc',$payment['ep_doc'])->first();
+          if(!$not) {
+            $not = new EasypayNotification;
+            $not->ep_doc = $payment['ep_doc'];
+            $not->ep_cin = $payment['ep_cin'];
+            $not->ep_user = $payment['ep_user'];
 
-        //if notification not processed, process it
-        if( $not->ep_status == 'ok0') {
-          $not->ep_status = 'processed';
-          $not->ep_entity = $payment['ep_entity'];
-          $not->ep_reference = $payment['ep_reference'];
-          $not->ep_value = $payment['ep_value'];
-          $not->t_key = ($payment['t_key'])?$payment['t_key']:'';
-          $not->ep_value = $payment['ep_value'];
-          $not->ep_payment_type = $payment['ep_payment_type'];
-          $not->ep_value_fixed = $payment['ep_value_fixed'];
-          $not->ep_value_var = $payment['ep_value_var'];
-          $not->ep_value_tax = $payment['ep_value_tax'];
-          $not->ep_value_transf = $payment['ep_value_transf'];
-          $not->ep_date_transf = $payment['ep_date_transf'];
-          $not->ep_date = $payment['ep_date'];
-          $not->save();
-          $ref = Reference::where('reference',$not->ep_reference)->first();
-          if($ref) {
-            //reference exists, set state 1 if needed
-            if($ref->state != 1) {
-              $ref->paid_value = $not->ep_value;
-              $ref->paid_date = $not->ep_date;
-              $ref->state=1;
-              $ref->save();
-              event(new PaymentReceived($ref->foreign_type, $ref->foreign_id, $not->ep_value));
-            }
-          } else {
-            //unknown reference, set notification status to no-reference
-            $not->ep_status = 'no-reference';
+            $not->ep_status = 'ok0';
             $not->save();
           }
-        }
+
+          //if notification not processed, process it
+          if( $not->ep_status == 'ok0') {
+            $not->ep_status = 'processed';
+            $not->ep_entity = $payment['ep_entity'];
+            $not->ep_reference = $payment['ep_reference'];
+            $not->ep_value = $payment['ep_value'];
+            $not->t_key = ($payment['t_key'])?$payment['t_key']:'';
+            $not->ep_value = $payment['ep_value'];
+            $not->ep_payment_type = $payment['ep_payment_type'];
+            $not->ep_value_fixed = $payment['ep_value_fixed'];
+            $not->ep_value_var = $payment['ep_value_var'];
+            $not->ep_value_tax = $payment['ep_value_tax'];
+            $not->ep_value_transf = $payment['ep_value_transf'];
+            $not->ep_date_transf = $payment['ep_date_transf'];
+            $not->ep_date = $payment['ep_payment_date'];
+            $not->save();
+            $ref = Reference::where('reference',$not->ep_reference)->first();
+            if($ref) {
+              //reference exists, set state 1 if needed
+              if($ref->state != 1) {
+                $ref->paid_value = $not->ep_value;
+                $ref->paid_date = $not->ep_date;
+                $ref->state=1;
+                $ref->save();
+                event(new PaymentReceived($ref->foreign_type, $ref->foreign_id, $not->ep_value));
+              }
+            } else {
+              //unknown reference, set notification status to no-reference
+              $not->ep_status = 'no-reference';
+              $not->save();
+            }
+          }
+      }
       }
     }
 }
